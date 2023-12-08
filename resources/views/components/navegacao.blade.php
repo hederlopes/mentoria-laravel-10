d97e6ee1f85c5a10820e78038748470de46240a8
hederlopes <sistemas.heder@gmail.com>
2023-12-07 21:00:11 -0300

    referenciar rota no front

--- resources/views/components/navegacao.blade.php
+++ resources/views/components/navegacao.blade.php
@@ -19,9 +19,9 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link d-flex align-items-center gap-2" href="#">
+            <a class="nav-link d-flex align-items-center gap-2" href="{{route('produto.index')}}">
               <svg class="bi"><use xlink:href="#cart"/></svg>
-              Profutos
+              Produtos
             </a>
           </li>
           <li class="nav-item">
