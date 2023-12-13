3e8a508ef122d6ec7a6f7afbc5b1e82fbed2abdd
hederlopes <sistemas.heder@gmail.com>
2023-12-13 16:31:57 -0300

    model produto

---
+++ database/seeders/ProdutosSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Produto;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProdutosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Produto::create(
+            [
+               'nome' => 'Heder Lopes',
+               'valor' => '20.00'
+            ]
+            );
+            
+    }
+}
